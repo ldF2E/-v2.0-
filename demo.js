6bcb4a9c4856c91758a026fd62c8de8348117b62
dereksunok <derek_sun_ok@163.com>
2018-01-26 11:24:29 +0800

    readme

--- demo.js
+++ demo.js
@@ -1,20 +1,20 @@
-(function(window, $ , undefined){
-	var _block_namespaces_ = window._block_namespaces_ || ( window._block_namespaces_ = {});
+(function(window, $, undefined) {
+    var _block_namespaces_ = window._block_namespaces_ || (window._block_namespaces_ = {});
     var name = "photoGallery";
     var photoGallery = _block_namespaces_[name] || (_block_namespaces_[name] = {});
     $.extend(photoGallery, {
-    	"init" : init
+        "init": init
     })
-    
-    function init(nodeObj){
+
+    function init(nodeObj) {
         if (!!!nodeObj) {
             return;
         }
-    	$(window).resize(function() { 
-            $('div[data-settingid=' + nodeObj.settingId + '] div[data-auto_uuid=' + nodeObj.nodeId + '] .sitewidget-common-gallery-detail').each(function(){
+        $(window).resize(function() {
+            $('div[data-settingid=' + nodeObj.settingId + '] div[data-auto_uuid=' + nodeObj.nodeId + '] .sitewidget-common-gallery-detail').each(function() {
                 $(this).height($(this).width());
             });
         });
         $(window).resize();
     }
-})(window,jQuery);
+})(window, jQuery);
